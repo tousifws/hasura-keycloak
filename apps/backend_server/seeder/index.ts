98d0dfc1eae5cd0ec2c9187e2e7bbcadb22e8edc
Tousif <82563595+tousifws@users.noreply.github.com>
2023-04-19 14:54:05 +0530

    Add seeder (#1)
    * feat: add seeder using kysely
    
    * doc: update readme

---
+++ apps/backend_server/seeder/index.ts
@@ -0,0 +1,47 @@
+import { Kysely, PostgresDialect } from 'kysely';
+import { DB } from 'kysely-codegen';
+import { Pool } from 'pg';
+import { randomUUID } from 'crypto';
+import { faker } from '@faker-js/faker';
+
+const db = new Kysely<DB>({
+    dialect: new PostgresDialect({
+        pool: new Pool({
+            connectionString: process.env.DATABASE_URL,
+        }),
+    }),
+});
+
+const userIds = [...Array(10).keys()].map(() => ({
+    id: randomUUID(),
+}));
+
+async function main() {
+    await db
+        .insertInto('users')
+        .values(
+            userIds.map(({ id }) => ({
+                id,
+                email: faker.internet.email(),
+                name: faker.name.fullName(),
+                phone: faker.phone.number(),
+            }))
+        )
+        .execute();
+
+    await db
+        .insertInto('posts')
+        .values(
+            userIds.map(({ id }) => ({
+                author: id,
+                title: faker.random.words(5),
+                content: faker.random.words(15),
+            }))
+        )
+        .execute();
+}
+
+main().catch(async (e) => {
+    console.error(e);
+    process.exit(1);
+});
